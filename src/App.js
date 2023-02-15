6c18f8377df1b8dc6d8e62bd41c4ecafe6726243
Nikhil Thakur <nikhil.thakur@incedoinc.com>
2023-02-15 23:06:41 +0530

    front end code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sign_up from './components/Sign_up/Form';
+import Navbar from './components/Navbar/Navbar';
+import {Route,Routes} from'react-router-dom'
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import Services from './components/Services/Services';
+import About from './components/About/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <Navbar></Navbar>
+      <Routes>
+      <Route exact path="/" element={<Home/>}>  </Route>
+      <Route exact path="/services" element={<Services/>}>  </Route>
+      <Route exact path="/about" element={<About/>}>  </Route>
+        <Route exact path="/login" element={<Login/>}>  </Route>
+        <Route  exact path="/SignUp" element={<Sign_up/>}></Route>
+        {/* <Redirect></Redirect> */}
+      </Routes>
+    
+        
+    </>
+    
   );
 }
 
